11f40452058e2c627e13922a574f1447023e7e0f
rodrigopdo <rodrigo.prudente82@gmail.com>
2024-07-08 11:26:13 -0300

    First commit

---
+++ src/main/java/com/pdo/etf_manager/EtfManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pdo.etf_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtfManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtfManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pdo/etf_manager/EtfManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdo.etf_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtfManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
